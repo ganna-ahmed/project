0025215393d419e6d186de053dc3e19fc3ba1aca
toka <tokaelswasay32@gmail.com>
2024-11-02 04:46:48 +0200

    new project

--- lib/features/question/presentation/views/widgets/custom_button.dart
+++ lib/features/question/presentation/views/widgets/custom_button.dart
@@ -25,3 +25,34 @@ class MoreButtonWidget extends StatelessWidget {
     );
   }
 }
+class DoneButton extends StatelessWidget {
+  final double buttonFontSize = 16;
+  final double buttonPaddingVertical = 10;
+  final double buttonPaddingHorizontal = 20;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {},
+      style: ElevatedButton.styleFrom(
+        backgroundColor: AppColors.ceruleanBlue,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+      ),
+      child: Padding(
+        padding: EdgeInsets.symmetric(
+          vertical: buttonPaddingVertical,
+          horizontal: buttonPaddingHorizontal,
+        ),
+        child: Text(
+          "Done",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: buttonFontSize,
+          ),
+        ),
+      ),
+    );
+  }
+}
