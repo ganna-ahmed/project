749a9c1956d2cf77450359fbd0a927c4fdfc7c85
tasneem-mohammed2 <tasneemgharib930@gmail.com>
2024-12-03 18:51:00 +0200

    Update home_view.dart

--- lib/features/home/presentation/views/home_view.dart
+++ lib/features/home/presentation/views/home_view.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:project/core/constants/colors.dart';
-import 'package:project/core/utils/app_router.dart';
 import 'package:project/core/utils/assets.dart';
+
+import '../../../../core/utils/app_router.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
@@ -10,11 +11,16 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: AppColors.white,
       appBar: AppBar(
-        backgroundColor: AppColors.wildBlueYonder,
+        backgroundColor: AppColors.white,
         centerTitle: true,
-        title: const Text(
-          'Home',
+        leading: Icon(
+          Icons.arrow_back,
+          color: AppColors.darkBlue,
+        ),
+        title: Text(
+          "Home",
           style: TextStyle(color: AppColors.darkBlue),
         ),
         actions: [
@@ -29,158 +35,122 @@ class HomeView extends StatelessWidget {
           )
         ],
       ),
-      backgroundColor: Colors.white,
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Container(
-            height: 400,
-            decoration: const BoxDecoration(
-              color: AppColors.wildBlueYonder,
-              borderRadius: BorderRadius.only(
-                bottomLeft: Radius.circular(150.0),
-                bottomRight: Radius.circular(150.0),
+      body: Padding(
+        padding: const EdgeInsets.all(25.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: 10,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                GoRouter.of(context).push(AppRouter.kBubbleShett);
+              },
+              child: Text(
+                "New Project",
+                style: TextStyle(color: AppColors.white, fontSize: 20),
+              ),
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: AppColors.ceruleanBlue,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(14),
+                  ),
+                  minimumSize: Size(130, 60)),
+            ),
+            SizedBox(height: 60),
+            Expanded(
+              child: ListView(
+                children: [
+                  ExamCard(
+                    title: "Exam One",
+                    questions: 20,
+                    professors: ["DR. Osama", "DR. Amr"],
+                    color: AppColors.stoneBlue,
+                    onTap: () {
+                      GoRouter.of(context).push(AppRouter.kAddQuestion);
+                    },
+                  ),
+                  ExamCard(
+                    title: "Exam Two",
+                    questions: 50,
+                    professors: ["DR. Osama"],
+                    color: AppColors.paleSky,
+                    onTap: () {
+                      GoRouter.of(context).push(AppRouter.kAddQuestion);
+                    },
+                  ),
+                  ExamCard(
+                    title: "Exam Three",
+                    questions: 100,
+                    professors: ["DR. Osama", "DR. Sara"],
+                    color: AppColors.babyBlue,
+                    onTap: () {
+                      GoRouter.of(context).push(AppRouter.kAddQuestion);
+                    },
+                  ),
+                ],
               ),
             ),
-            padding:
-                const EdgeInsets.symmetric(vertical: 30.0, horizontal: 20.0),
-            child: Column(
-              children: [
-                const SizedBox(height: 60),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: AppColors.ceruleanBlue,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(16),
-                        ),
-                        padding: const EdgeInsets.symmetric(
-                          vertical: 40.0,
-                          horizontal: 5.0,
-                        ),
-                        minimumSize: const Size(160, 60),
-                      ),
-                      onPressed: () {
-                        GoRouter.of(context).push(AppRouter.kUploadView);
-                      },
-                      child: const Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(
-                            'Update',
-                            style: TextStyle(fontSize: 28, color: Colors.white),
-                          ),
-                          Text(
-                            'Project',
-                            style: TextStyle(fontSize: 28, color: Colors.white),
-                          ),
-                        ],
-                      ),
-                    ),
-                    ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: AppColors.ceruleanBlue,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(16),
-                        ),
-                        padding: const EdgeInsets.symmetric(
-                          vertical: 40.0,
-                          horizontal: 5.0,
-                        ),
-                        minimumSize: const Size(160, 60),
-                      ),
-                      onPressed: () {
-                        GoRouter.of(context).push(AppRouter.kAddQuestion);
-                      },
-                      child: const Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(
-                            'New',
-                            style: TextStyle(fontSize: 28, color: Colors.white),
-                          ),
-                          Text(
-                            'Project',
-                            style: TextStyle(fontSize: 28, color: Colors.white),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ],
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ExamCard extends StatelessWidget {
+  final String title;
+  final int questions;
+  final List<String> professors;
+  final Color color;
+  final VoidCallback onTap;
+
+  const ExamCard({
+    required this.title,
+    required this.questions,
+    required this.professors,
+    required this.color,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+        margin: EdgeInsets.only(left: 2, bottom: 19, right: 30),
+        padding: EdgeInsets.all(15),
+        decoration: BoxDecoration(
+          color: color,
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              title,
+              style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: AppColors.white),
             ),
-          ),
-          const SizedBox(height: 40),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20.0),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: AppColors.ceruleanBlue,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(16),
-                    ),
-                    padding: const EdgeInsets.symmetric(vertical: 20.0),
-                    minimumSize: const Size(double.infinity, 48),
-                  ),
-                  onPressed: () {
-                    GoRouter.of(context).push(AppRouter.kBubbleShett);
-                  },
-                  child: const Text(
-                    'Create Bubble Sheet',
-                    style: TextStyle(
-                      fontSize: 22,
-                      color: AppColors.snow,
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 20),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: AppColors.ceruleanBlue,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(16),
-                    ),
-                    padding: const EdgeInsets.symmetric(vertical: 20.0),
-                    minimumSize: const Size(double.infinity, 48),
-                  ),
-                  onPressed: () {},
-                  child: const Text(
-                    'Creat Models Of Question',
-                    style: TextStyle(
-                      fontSize: 22,
-                      color: AppColors.snow,
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 20),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: AppColors.ceruleanBlue,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(16),
-                    ),
-                    padding: const EdgeInsets.symmetric(vertical: 20.0),
-                    minimumSize: const Size(double.infinity, 48),
-                  ),
-                  onPressed: () {},
-                  child: const Text(
-                    'Correction Of Bubble Sheet',
-                    style: TextStyle(
-                      fontSize: 22,
-                      color: AppColors.snow,
-                    ),
-                  ),
-                ),
-              ],
+            Text(
+              "$questions questions",
+              style: TextStyle(fontSize: 16, color: AppColors.white),
             ),
-          ),
-        ],
+            SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: professors
+                  .map((professor) => Text(
+                        professor,
+                        style: TextStyle(fontSize: 16, color: AppColors.white),
+                      ))
+                  .toList(),
+            ),
+          ],
+        ),
       ),
     );
   }
